cb22407ef09bc3ba5198fadc334bd2f4095e5b34
jtmb <james.branco@gmail.com>
2021-08-19 19:26:22 -0400

    commit

---
+++ pivpn-runner.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+docker pull ljishen/pivpn
+
+docker run -d --name pivpn-server -ti --rm \
+     --privileged \
+     -p 443:443/udp \
+     -v "$HOME"/ovpns:/home/pivpn/ovpns \
+     ljishen/pivpn
+
+
+#  sudo apt-get install openvpn 
+
+#  sudo openvpn --auth-nocache --config client.ovpn
