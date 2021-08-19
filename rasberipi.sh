2deb75c9b8bf98f6e5e79bcd113e0152d7a86ef0
jtmb <james.branco@gmail.com>
2021-08-19 15:38:45 -0400

    Initial Commit

--- rasberipi.sh
+++ rasberipi.sh
@@ -1,20 +1,22 @@
 #!bin/bash
 
 echo Installing rasberypi autoconfig.
-
+sleep 3
 echo Installing Jenkins
+
+# Setup Jenkins Home Dir working folder
+cd /home/james
+mkdir jenkins_home
 
 sudo docker pull mlucken/jenkins-arm
 
-sudo docker run -d --name jenkins-server2 -p 8090:8080 -p 50000:50000 -v /home/jenkins_home:/var/jenkins_home mlucken/jenkins-arm;
+sudo docker run -d --name jenkins-server2 -p 8090:8080 -p 50000:50000 -v /home/james/jenkins_home:/var/jenkins_home mlucken/jenkins-arm;
 
 sudo docker start jenkins-server2
-
-echo Printing Jenkins password
-sudo docker exec -it jenkins-server2 cat /var/jenkins_home/secrets/initialAdminPassword
-echo Jenkins unlock password above. Save this You;ll need it for the next step!
 
 #Set Jebkins docker instance to restart automatic on system reboot
 sudo docker update --restart unless-stopped jenkins-server2
 
-
+echo URL: localhost:8090 or 192.168.0.19:8090
+echo Jenkins Password bellow!!
+sudo docker exec -it jenkins-server2 cat /var/jenkins_home/secrets/initialAdminPassword
