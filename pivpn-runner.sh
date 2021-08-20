e5dd99120f03c61d23003ce9cc518a3681048973
jtmb <james.branco@gmail.com>
2021-08-19 20:07:17 -0400

    Initial Commit

--- pivpn-runner.sh
+++ pivpn-runner.sh
@@ -1,14 +1,20 @@
 #!bin/bash
 
-docker pull ljishen/pivpn
-
-docker run --name pivpn-server -ti --rm \
-     --privileged \
-     -p 443:443/udp \
-     -v "$HOME"/ovpns:/home/pivpn/ovpns \
-     ljishen/pivpn
+# Create local IP variable and bind to show default local interface IP
+mylocalip=$(sudo ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+# Create Public IP variable and bind to show default Public IP
+mypublicip=$(curl https://ipinfo.io/ip)
 
 
-#  sudo apt-get install openvpn 
 
-#  sudo openvpn --auth-nocache --config client.ovpn
+# Step 4: Set up the VPN container
+
+OVPN_DATA="ovpn-data" 
+sudo docker volume create --name $OVPN_DATA
+sudo docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm mjenz/rpi-openvpn ovpn_genconfig -u udp://$mypublicip
+sudo docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm -it mjenz/rpi-openvpn ovpn_initpki
+sudo docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN --restart always mjenz/rpi-openvpn
+
+# Start VPN Container and build certificate
+sudo docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm -it mjenz/rpi-openvpn easyrsa build-client-full CLIENTNAME nopass
+sudo docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm mjenz/rpi-openvpn ovpn_getclient CLIENTNAME > CLIENTNAME.ovpn
