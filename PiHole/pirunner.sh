03d109a94fe405e09bab1fde4caa528c5d2ff7ba
jtmb <james.branco@gmail.com>
2021-08-22 20:36:01 -0400

    yes

--- PiHole/pirunner.sh
+++ PiHole/pirunner.sh
@@ -8,3 +8,18 @@ sudo pip3 install docker-compose
 
 # Run container
 docker-compose up --detach
+
+echo
+echo
+echo ----------------- Pihole URLS---------------------------
+echo
+echo Local Addresse: "$mylocalip:8091" 
+echo Public Address: "$mypublicip:8091"
+echo
+echo PiHole Admin Password:
+sudo docker exec -it pihole sudo pihole -a -p
+echo
+echo -------------------------------------------------------------------
+echo
+echo
+echo Finished!
