a4470606a2da45f5f16452fc2365e3877bdc2465
jtmb <james.branco@gmail.com>
2021-08-22 20:24:20 -0400

    timezone updates

--- PiHole/pirunner.sh
+++ PiHole/pirunner.sh
@@ -1,16 +1,10 @@
 #!bin/bash
 
-# Builds Docker image and destroys old one
-sudo docker build -t pihole_image .
-sudo docker rm -f pihole-server
+# system host prereqs
+sudo apt-get install libffi-dev libssl-dev
+sudo apt install python3-dev
+sudo apt-get install -y python3 python3-pip
+sudo pip3 install docker-compose
 
-# Runs Docker image in silent mode and in privilleged mode
-sudo docker run -d -it --name pihole-server pihole_image bash
-
-# Install Dependencies inside container
-sudo docker exec -it pihole-server apt-get -y install wget
-sudo docker exec -it pihole-server apt update
-sudo docker exec -it pihole-server apt -y install iproute2
-sudo docker exec -it pihole-server apt -y curl -sSL https://install.pi-hole.net | bash
-
-
+# Run container
+docker-compose up --detach
