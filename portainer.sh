cba81f491dccd4805c69fe2f4ce919724d1aecd2
jtmb <86915618+jtmb@users.noreply.github.com>
2021-08-24 13:25:30 -0400

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -18,7 +18,8 @@ sudo docker rm -f portainer
 sudo docker volume create portainer_data
 
 docker run -d -p 8090:8000 -p 9090:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
-  
+
+
 
 echo
 echo
@@ -26,6 +27,8 @@ echo ----------------- Portainer URLS---------------------------
 echo
 echo Local Address: "$mylocalip:32400/manage"
 echo
+echo run Run the following command to deploy the Agent in your Docker host:
+echo docker run -d -p 9091:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent
 echo -------------------------------------------------------------------
 echo
 echo
