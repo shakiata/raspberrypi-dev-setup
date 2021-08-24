95a99f4772451df7751ec2aa5c0d08ad989e50ee
jtmb <86915618+jtmb@users.noreply.github.com>
2021-08-24 12:02:51 -0400

    Update plexrunner.sh

--- plexrunner.sh
+++ plexrunner.sh
@@ -15,30 +15,40 @@ sudo mkdir database; sudo mkdir transcode; sudo mkdir plexmedia
 cd;
 
 # Pull/Run container
-sudo docker pull plexinc/pms-docker
+sudo docker pull linuxserver/plex
 # sudo docker run -d --name plex-server -p 8093:32400 -v plex/transcode:/transcode -v plex/database:/config plexinc/pms-docker;
 
 sudo rm -f plex
 
-sudo docker run \
--d \
---name plex \
--p 32400:32400/tcp \
--p 3005:3005/tcp \
--p 8324:8324/tcp \
--p 32469:32469/tcp \
--p 1900:1900/udp \
--p 32410:32410/udp \
--p 32412:32412/udp \
--p 32413:32413/udp \
--p 32414:32414/udp \
--e TZ="America/Toronto" \
--e ADVERTISE_IP="http://192.168.0.29:32400/" \
--v /home/james/plex/database:/config \
--v /home/james/plex/transcode:/transcode \
--v /home/james/plex/plexmedia:/data \
---restart unless-stopped \
-plexinc/pms-docker
+# sudo docker run \
+# -d \
+# --name plex \
+# -p 32400:32400/tcp \
+# -p 3005:3005/tcp \
+# -p 8324:8324/tcp \
+# -p 32469:32469/tcp \
+# -p 1900:1900/udp \
+# -p 32410:32410/udp \
+# -p 32412:32412/udp \
+# -p 32413:32413/udp \
+# -p 32414:32414/udp \
+# -e TZ="America/Toronto" \
+# -e ADVERTISE_IP="http://192.168.0.29:32400/" \
+# --restart unless-stopped \
+# linuxserver/plex
+
+
+sudo docker run -d \
+  --name=plex \
+  --net=host \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e VERSION=docker \
+  -v /home/james/plex/database:/config \
+  -v /home/james/plex/transcode:/transcode \
+  -v /home/james/plex/plexmedia:/data \
+  --restart unless-stopped \
+  ghcr.io/linuxserver/plex
 
 
 
