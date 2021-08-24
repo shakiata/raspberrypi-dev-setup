f71266cdd4f0891d2303da7af7ae93f4ef1dfbb2
jtmb <86915618+jtmb@users.noreply.github.com>
2021-08-24 12:44:23 -0400

    Update plexrunner.sh

--- plexrunner.sh
+++ plexrunner.sh
@@ -16,27 +16,8 @@ cd;
 
 # Pull/Run container
 sudo docker pull linuxserver/plex
-# sudo docker run -d --name plex-server -p 8093:32400 -v plex/transcode:/transcode -v plex/database:/config plexinc/pms-docker;
-
+# remove old images
 sudo docker rm -f plex
-
-# sudo docker run \
-# -d \
-# --name plex \
-# -p 32400:32400/tcp \
-# -p 3005:3005/tcp \
-# -p 8324:8324/tcp \
-# -p 32469:32469/tcp \
-# -p 1900:1900/udp \
-# -p 32410:32410/udp \
-# -p 32412:32412/udp \
-# -p 32413:32413/udp \
-# -p 32414:32414/udp \
-# -e TZ="America/Toronto" \
-# -e ADVERTISE_IP="http://192.168.0.29:32400/" \
-# --restart unless-stopped \
-# linuxserver/plex
-
 
 sudo docker run -d \
   --name=plex \
@@ -58,10 +39,7 @@ echo
 echo
 echo ----------------- Plex URLS---------------------------
 echo
-echo Local Address: "$mylocalip:32400"
-echo
-# echo PiHole Admin Password:
-# sudo docker exec -it pihole sudo pihole -a -p
+echo Local Address: "$mylocalip:32400/manage"
 echo
 echo -------------------------------------------------------------------
 echo
