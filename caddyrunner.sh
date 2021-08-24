6fd4350f95e2f42c45c137c6915759fe4abdeeeb
jtmb <86915618+jtmb@users.noreply.github.com>
2021-08-24 16:23:00 -0400

    Update caddyrunner.sh

--- caddyrunner.sh
+++ caddyrunner.sh
@@ -17,11 +17,11 @@ sudo chmod 777 caddy_data
 
 echo "hello world" > index.html
 docker run -d -p 8080:80 --name=caddy-webserver \
-    -v /home/james/websites:/usr/share/caddy/index.html \
+    -v /home/james/websites:/index.html \
     -v /home/james/caddy_data:/data \
     caddy
 
-curl http://localhost/
+curl http://localhost/8080
 
 echo
 echo
