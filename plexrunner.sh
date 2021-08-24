6fa5b8f52a5e917898a55ee959b8b79300db06bc
jtmb <86915618+jtmb@users.noreply.github.com>
2021-08-24 09:11:07 -0400

    Added run commands

--- plexrunner.sh
+++ plexrunner.sh
@@ -17,7 +17,25 @@ mylocalip=$(sudo ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ 
 # Pull/Run container
 docker pull plexinc/pms-docker
 # sudo docker run -d --name plex-server -p 8093:32400 -v plex/transcode:/transcode -v plex/database:/config plexinc/pms-docker;
-sudo docker run -d --name plex-server -p 8093:32400 plexinc/pms-docker;
+
+docker run \
+-d \
+--name plex \
+-p 32400:32400/tcp \
+-p 3005:3005/tcp \
+-p 8324:8324/tcp \
+-p 32469:32469/tcp \
+-p 1900:1900/udp \
+-p 32410:32410/udp \
+-p 32412:32412/udp \
+-p 32413:32413/udp \
+-p 32414:32414/udp \
+-e TZ="<timezone>" \
+-e ADVERTISE_IP="http://192.168.0.29:32400/" \
+-v plex/database>:/config \
+-v transcode/temp>:/transcode \
+-v plexmedia:/data \
+plexinc/pms-docker
 
 
 
