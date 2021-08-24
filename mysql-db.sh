9d5a17eb58e128f5b533c25f57493ef0048b944e
jtmb <86915618+jtmb@users.noreply.github.com>
2021-08-24 18:56:52 -0400

    Update mysql-db.sh

--- mysql-db.sh
+++ mysql-db.sh
@@ -4,9 +4,11 @@
 # Create local IP variable and bind to show default local interface IP
 mylocalip=$(sudo ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 
-docker pull mysql
+docker pull mariadb
 
-docker run --name some-mysql -p 8094:8080 -e MYSQL_ROOT_PASSWORD=8108453500! -d mysql
+# docker run --name some-mysql -p 8094:8080 -e MYSQL_ROOT_PASSWORD=8108453500! -d mysql
+
+docker run -it --name some-mysql -p 8094:8080 --rm mariadb mysql -hsome-mariadb -admin -p
 
 echo
 echo
