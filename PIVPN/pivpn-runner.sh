58bb200d2fd9417e0d6dd270b677869390fab522
jtmb <86915618+jtmb@users.noreply.github.com>
2021-08-25 15:27:36 -0400

    Update pivpn-runner.sh

--- PIVPN/pivpn-runner.sh
+++ PIVPN/pivpn-runner.sh
@@ -12,11 +12,11 @@ OVPN_DATA="ovpn-data"
 sudo docker volume create --name $OVPN_DATA
 sudo docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm mjenz/rpi-openvpn ovpn_genconfig -u udp://$mypublicip
 sudo docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm -it mjenz/rpi-openvpn ovpn_initpki
-sudo docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN --restart always mjenz/rpi-openvpn
+sudo docker run --name ovpn-server -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN --restart always mjenz/rpi-openvpn
 
 # Start VPN Container and build certificate
 sudo docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm -it mjenz/rpi-openvpn easyrsa build-client-full CLIENTNAME nopass
 sudo docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm mjenz/rpi-openvpn ovpn_getclient CLIENTNAME > CLIENTNAME.ovpn
 
 
-sudo docker exec -it blissful_cannon cat /etc/openvpn/pki/private/CLIENTNAME.key.XXXXPmAALe
+sudo docker exec -it ovepn-server cat /etc/openvpn/pki/private/CLIENTNAME.key.XXXXPmAALe
