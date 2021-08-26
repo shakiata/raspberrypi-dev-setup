b910694b6349c6a698f1fa1284394c7cb49d00fb
jtmb <86915618+jtmb@users.noreply.github.com>
2021-08-26 12:49:07 -0400

    Update glancesrunner.sh

--- glancesrunner.sh
+++ glancesrunner.sh
@@ -16,19 +16,18 @@ sudo docker pull nicolargo/glances
 sudo docker rm -f glances-sys-monitor
 
 # Launch Portainer
-sudo docker run --name glances-sys-monitor -d --restart="always" -p 61208-61209:61208-61209 -e GLANCES_OPT="-w" -v /var/run/docker.sock:/var/run/docker.sock:ro --pid host nicolargo/glances
+sudo docker run --name glances-sys-monitor -d --restart="always" -p 8093-8093:61208-61209 -e GLANCES_OPT="-w" -v /var/run/docker.sock:/var/run/docker.sock:ro --pid host nicolargo/glances
 
 
 sudo docker update --restart unless-stopped glances-sys-monitor
 
 echo
 echo
-echo ----------------- Portainer URLS---------------------------
+echo ----------------- Glances URLS---------------------------
 echo
-echo Local Address: "$mylocalip:8090"
+echo Local Address: "$mylocalip:8093"
 echo
-echo run Run the following command to deploy the Agent in your Docker host:
-echo docker run -d -p 9091:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent
+echo
 echo -------------------------------------------------------------------
 echo
 echo
