e5df9ad5980bf58cae6725b893075c5de5b4a7b1
jtmb <86915618+jtmb@users.noreply.github.com>
2022-01-03 09:09:34 -0500

    Update portaineragent.sh

--- portaineragent.sh
+++ portaineragent.sh
@@ -1,2 +1,2 @@
-docker rm -f portainer_agent
-docker run -d -p 9091:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent
+sudo docker rm -f portainer_agent
+sudo docker run -d -p 9091:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent
