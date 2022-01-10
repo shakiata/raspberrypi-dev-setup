c3687838fe9a89fbfa36124de8e554a8a1a2d2f9
jtmb <james.branco@gmail.com>
2022-01-10 09:09:03 -0500

    added homer config

---
+++ Homer/homer.sh
@@ -0,0 +1,15 @@
+usrdir=$(eval echo ~$USER)
+
+
+docker run -d \
+  --name homer \
+  -p 8081:8080 \
+  -v /home/homer/assets/:/www/assets \
+  --restart=always \
+  b4bz/homer:latest
+
+
+cp /home/james/raspberrypi-dev-setup/Homer/config.yml /home/homer/assets/config.yml
+
+
+  echo Finished :8081
